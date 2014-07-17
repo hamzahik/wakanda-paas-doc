b0e2b64dfb81226b04bc4b0ab2dfadba890ac60d
Mohamed IDRISSI <idrissi@Mohameds-iMac.local>
2014-07-17 12:49:00 +0000

    Change the title

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'wakanda-paas'
+project = u'Wakanda Cloud'
 copyright = u'2014, Wakanda'
 
 # The version info for the project you're documenting, acts as replacement for
